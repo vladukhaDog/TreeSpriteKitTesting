9fa5669755ea13548da759c17d5491805556b406
vladukhaDog <vladic.p@gmail.com>
2022-11-22 16:39:33 +0300

    Initial Commit

---
+++ TreeSpriteKitTesting/TreeSpriteKitTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreeSpriteKitTestingApp.swift
+//  TreeSpriteKitTesting
+//
+//  Created by Permyakov Vladislav on 22.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct TreeSpriteKitTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
